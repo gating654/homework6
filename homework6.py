cbaa6dc46c767d87cee80db3230a2fb173de3b7c
gating654 <168896707+gating654@users.noreply.github.com>
2024-05-15 21:47:27 +0700

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,9 @@
+auto = "Я езжу на автомабиле марки "
+list_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for i in list_:
+    a = auto + i
+    cars_count += 10
+    print(f"{a} x {cars_count}")
+
+
